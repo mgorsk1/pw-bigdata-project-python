cf3dadcd2d23f9b0e46ee01881872eeec36bbf22
mgorsk1 <gorskimariusz13@gmail.com>
2018-10-31 22:41:18 +0100

    feat(socker reader): finished class reading data from socket and sending it to pubsub

--- app/socker_reader/main.py
+++ app/socker_reader/main.py
@@ -1,53 +1,67 @@
 import websocket
-from app.gcp_pubsub.publish import publish_message
-from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
+
+from app.gcp_pubsub.publish import PubSubPublisher
+from time import sleep
+
+from config import PROJECT_ID
 
 try:
     import thread
 except ImportError:
     import _thread as thread
 
-HOST = 'localhost'
-PORT = 9999
-ADDR = (HOST, PORT)
-tcpSock = socket(AF_INET, SOCK_STREAM)
-tcpSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-tcpSock.bind(ADDR)
-tcpSock.listen(5)
 
+class WebsocketToPubSubEmitter:
+    def __init__(self, ws_url_arg, project_id_arg, topic_arg, seconds_arg=None):
+        self.publisher = PubSubPublisher(project_id_arg, topic_arg)
+        self.url = ws_url_arg
+        self.seconds=seconds_arg
 
-class MeetupSocketHandler:
-    url = "ws://stream.meetup.com/2/rsvps"
+        self.ws = None
 
-    @staticmethod
-    def on_message(ws, message):
-        publish_message("pw-bigdata-final-project", "meetup-rawdata", message)
+        self.run()
 
-    @staticmethod
-    def on_error(ws, error):
+    def run(self):
+        websocket.enableTrace(True)
+        self.ws = websocket.WebSocketApp(self.url,
+                                         on_message=self.on_message,
+                                         on_error=self.on_error,
+                                         on_close=self.on_close)
+
+        self.ws.counter = 0
+
+        self.ws.on_open = self.on_open
+        self.ws.run_forever()
+
+    def on_message(self, message):
+        self.publisher.publish_message(message)
+        self.ws.counter += 1
+
+    def on_error(self, error):
         print(error)
 
-    @staticmethod
-    def on_close(ws):
+    def on_close(self):
         print("### closed ###")
 
-    @staticmethod
-    def on_open(ws):
+    def on_open(self):
         def run(*args):
-            pass
-            # time.sleep(120)
-            # ws.close()
-            # print("thread terminating...")
+            if self.seconds:
+                print("Running for {} seconds...".format(self.seconds))
+                sleep(self.seconds)
+                self.ws.close()
+                print("Published {} messages in {} seconds. That is {} mps!".format(self.counter,
+                                                                                    self.seconds,
+                                                                                    self.counter/self.seconds))
+
+            else:
+                print("Running forever...")
+                while True:
+                    pass
 
         thread.start_new_thread(run, ())
 
+    def return_count(self):
+        return self.ws.counter
 
-websocket.enableTrace(True)
-ws = websocket.WebSocketApp(MeetupSocketHandler.url,
-                            on_message=MeetupSocketHandler.on_message,
-                            on_error=MeetupSocketHandler.on_error,
-                            on_close=MeetupSocketHandler.on_close)
 
-ws.on_open = MeetupSocketHandler.on_open
-# @todo run only in selected time period (for example: only through whole january)
-ws.run_forever()
+fire = WebsocketToPubSubEmitter("ws://stream.meetup.com/2/rsvps", PROJECT_ID, "meetup-rawdata", None)
